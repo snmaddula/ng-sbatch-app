0b6ab215323392d1c646979d4766dcd4a3033c70
Narayan Sambi Reddy <narayan-sambireddy@outlook.com>
2018-09-28 09:51:54 +0530

    Initial commit

---
+++ src/main/java/snmaddula/ngsbatch/app/NgSbatchAppApplication.java
@@ -0,0 +1,12 @@
+package snmaddula.ngsbatch.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NgSbatchAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NgSbatchAppApplication.class, args);
+	}
+}
