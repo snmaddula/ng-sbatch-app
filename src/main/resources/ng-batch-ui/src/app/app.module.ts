8c86c2fd882203b9ba7da4590e4d0630abd53437
snmaddula <sn_maddula@outlook.com>
2018-09-28 21:55:07 +0530

    v3

--- src/main/resources/ng-batch-ui/src/app/app.module.ts
+++ src/main/resources/ng-batch-ui/src/app/app.module.ts
@@ -3,17 +3,13 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { ListUploadComponent } from './upload/list-upload/list-upload.component';
 import { FormUploadComponent } from './upload/form-upload/form-upload.component';
-import { DetailsUploadComponent } from './upload/details-upload/details-upload.component';
 import { UploadFileService } from './upload/upload-file.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListUploadComponent,
-    FormUploadComponent,
-    DetailsUploadComponent
+    FormUploadComponent
   ],
   imports: [
     BrowserModule,
